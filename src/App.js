754f7085dde3bf5d83a8322ed10bd7f021d4830f
bestsupperbear <thongchai.mun@gmail.com>
2018-07-01 10:11:05 +0700

    commit message

--- src/App.js
+++ src/App.js
@@ -6,38 +6,39 @@ import ListItem from "./ListItem";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-        todos: [
-            {id: 1, name: "Eatting", Component: false },
-            {id: 2, name: "Sleepping", Component: false },
-            {id: 3, name: "Watch Movie", Component: false },
-            {id: 4, name: "Dinner", Component: false },
-        ],
-        message:""
-     };
+    this.state = {
+      todos: [
+        { id: 1, name: "Eatting", Component: false },
+        { id: 2, name: "Sleepping", Component: false },
+        { id: 3, name: "Watch Movie", Component: false },
+        { id: 4, name: "Dinner", Component: false }
+      ],
+      message: ""
+    };
 
-     this.onChangeMessage = this.onChangeMessage.bind(this);
-     this.onSubmitMessage = this.onSubmitMessage.bind(this);
-}
+    this.onChangeMessage = this.onChangeMessage.bind(this);
+    this.onSubmitMessage = this.onSubmitMessage.bind(this);
+  }
 
-onChangeMessage(e){//เพิ่มข้อมูล
-  this.setState({message: e.target.value});
-}
+  onChangeMessage(e) {
+    //เพิ่มข้อมูล
+    this.setState({ message: e.target.value });
+  }
 
-onSubmitMessage(e){
-  //ป้องกันการรีโหลดหน้า
-  e.preventDefault();
-  let oldTodos = this.state.todos;
-  let todoLength = this.state.todos.length;
-  let lastId = this.state.todos[todoLength -1].id;
-  let newMessage = {
-    id: lastId + 1,
-    name: this.state.message,
-    complete: false
-  };
-  oldTodos.push(newMessage);
-  this.setState({todos: oldTodos})
-}
+  onSubmitMessage(e) {
+    //ป้องกันการรีโหลดหน้า
+    e.preventDefault();
+    let oldTodos = this.state.todos;
+    let todoLength = this.state.todos.length;
+    let lastId = this.state.todos[todoLength - 1].id;
+    let newMessage = {
+      id: lastId + 1,
+      name: this.state.message,
+      complete: false
+    };
+    oldTodos.push(newMessage);
+    this.setState({ todos: oldTodos });
+  }
 
   render() {
     return (
@@ -52,11 +53,11 @@ onSubmitMessage(e){
           marginTop: 20
         }}
       >
-        <HeaderComponent/>
-        <List todos={this.state.todos}/>          
-       <FormSubmit  
-                    onChangeMessage={this.onChangeMessage} 
-                    onSubmitMessage={this.onSubmitMessage}
+        <HeaderComponent />
+        <List todos={this.state.todos} />
+        <FormSubmit
+          onChangeMessage={this.onChangeMessage}
+          onSubmitMessage={this.onSubmitMessage}
         />
       </div>
     );
